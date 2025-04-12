c113a523e68d9b7cfe7478165d653184d209a18c
TyagiManshi <166705490+TyagiManshi@users.noreply.github.com>
2025-04-12 19:25:56 +0530

    chore: setup basic Express server with dotenv, cors.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
 
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
