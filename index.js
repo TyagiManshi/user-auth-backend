2d230e78c52613b83ca2ad9f9872f51c8a33f7be
TyagiManshi <166705490+TyagiManshi@users.noreply.github.com>
2025-04-15 01:02:10 +0530

    feat: added functionality for login and verify routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import db from "./utils/db.js";
+import cookieParser from "cookie-parser";
 
 // import all routes 
 import userRoutes from "./routes/user.routes.js"
@@ -19,14 +20,11 @@ app.use(
   })
 );
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const port = process.env.PORT || 4000;
-
-app.get("/", (req, res) => { 
-  res.send("Hello World!");
-});
 
 db();
 
